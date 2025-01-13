b69e166fc51b9f7dfbb93be815a25822223e5c0a
Hirusha <157493986+hirushaph@users.noreply.github.com>
2025-01-13 12:39:26 +0530

    add animation to modal

---
+++ app/_components/Button.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+interface ButtonProps {
+  onClick: () => void;
+  children: React.ReactNode;
+  variant: "success" | "cancel" | "danger" | "done";
+}
+
+function Button({ onClick, children, variant }: ButtonProps) {
+  const baseClasses =
+    "px-4 py-[6px] uppercase text-[14px] font-semibold rounded-md transition-colors duration-200";
+  const variantClasses = {
+    success:
+      "bg-blue-100 border-blue-200 text-blue-600 hover:bg-blue-200 hover:border-blue-300 hover:text-blue-700",
+    cancel:
+      "bg-red-100 border-red-200 text-red-600 hover:bg-red-200 hover:border-red-300 hover:text-red-700",
+    danger:
+      "bg-yellow-100 border-yellow-200 text-yellow-600 hover:bg-yellow-200 hover:border-yellow-300 hover:text-yellow-700",
+    done: "bg-green-100 border-green-200 text-green-600 hover:bg-green-200 hover:border-green-300 hover:text-green-700",
+  };
+
+  return (
+    <button
+      className={`${baseClasses} ${variantClasses[variant]}`}
+      onClick={onClick}
+    >
+      {children}
+    </button>
+  );
+}
+
+export default Button;
--- app/_components/Modal.tsx
+++ app/_components/Modal.tsx
@@ -3,6 +3,8 @@ import { StockItemType } from "@/types/types";
 import { useEffect, useRef, useState } from "react";
 import { IoClose } from "react-icons/io5";
 import { createPortal } from "react-dom";
+import Button from "./Button";
+import { motion, AnimatePresence } from "framer-motion";
 
 type ModalProps = {
   isModalOpen: boolean;
@@ -32,66 +34,89 @@ function Modal({ item, isModalOpen, setIsModalOpen }: ModalProps) {
   }
 
   return createPortal(
-    <dialog
-      ref={dialogRef}
-      className="bg-white shadow-md w-[600px] p-3 z-50 backdrop:bg-black/75"
-    >
-      <div className="flex justify-between items-center">
-        <h1 className="text-xl font-semibold">Issue Item</h1>
-        <button onClick={() => onCloseModal()}>
-          <IoClose size={22} />
-        </button>
-      </div>
-      <div className="mt-4 body grid grid-cols-[1fr_2fr] py-4 gap-3">
-        <div className="border rounded-md p-2">
-          <img src={item?.image} className="rounded-md" alt="product image" />
-          <h2 className="text-sm font-semibold mt-2 text-gray-700 text-center">
-            {item?.name}
-          </h2>
-          <p className="text-sm font-semibold mt-2 text-gray-600 py-1 px-2 rounded-2xl bg-green-100 text-center">
-            Stock :{" "}
-            <span className="text-green-600 font-semibold">{item?.stock}</span>
-          </p>
-        </div>
-        <div>
-          <h3 className="text-sm text-gray-500">Issued to</h3>
-          <input
-            className="px-4 py-1 mt-2 border bg-slate-200 rounded-md outline-none focus:ring-2 focus:ring-blue-400 transition text-sm font-normal w-full"
-            type="search"
-            placeholder="search user"
+    <AnimatePresence>
+      {isModalOpen && (
+        <>
+          <motion.div
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 0.75 }}
+            exit={{ opacity: 0 }}
+            transition={{ duration: 0.3 }}
+            className="fixed inset-0 bg-black"
           />
-          <div className="t gap-2 mt-2 bg-green-100 p-2 rounded-md">
-            <span className="text-[12px] uppercase text-green-700 font-semibold">
-              Selected User
-            </span>
-            <p className="text-[14px] uppercase text-gray-500">
-              ID: <span className="text-gray-700">002</span>
-            </p>
-            <p className="text-[14px] uppercase text-gray-500">
-              Name: <span className="text-gray-700">John Doe</span>
-            </p>
-          </div>
+          <motion.dialog
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+            transition={{ duration: 0.3 }}
+            ref={dialogRef}
+            className="bg-white shadow-md w-[600px] p-4 rounded-md z-50 fixed inset-0 m-auto"
+          >
+            <div className="flex justify-between items-center">
+              <h1 className="text-xl font-semibold">Issue Item</h1>
+              <button onClick={() => onCloseModal()}>
+                <IoClose size={22} />
+              </button>
+            </div>
+            <div className="mt-4 body grid grid-cols-[1fr_2fr] py-4 gap-3">
+              <div className="border rounded-md p-2">
+                <img
+                  src={item?.image}
+                  className="rounded-md"
+                  alt="product image"
+                />
+                <h2 className="text-sm font-semibold mt-2 text-gray-700 text-center">
+                  {item?.name}
+                </h2>
+                <p className="text-sm font-semibold mt-2 text-gray-600 py-1 px-2 rounded-2xl bg-green-100 text-center">
+                  Stock :{" "}
+                  <span className="text-green-600 font-semibold">
+                    {item?.stock}
+                  </span>
+                </p>
+              </div>
+              <div>
+                <h3 className="text-sm text-gray-500">Issued to</h3>
+                <input
+                  className="px-4 py-1 mt-2 border bg-slate-200 rounded-md outline-none focus:ring-2 focus:ring-blue-400 transition text-sm font-normal w-full"
+                  type="search"
+                  placeholder="search user"
+                />
+                <div className="t gap-2 mt-2 bg-green-100 p-2 rounded-md">
+                  <span className="text-[12px] uppercase text-green-700 font-semibold">
+                    Selected User
+                  </span>
+                  <p className="text-[14px] uppercase text-gray-500">
+                    ID: <span className="text-gray-700">002</span>
+                  </p>
+                  <p className="text-[14px] uppercase text-gray-500">
+                    Name: <span className="text-gray-700">John Doe</span>
+                  </p>
+                </div>
 
-          <div>
-            <h3 className="text-sm text-gray-500 mt-2">Quantity</h3>
-            <input
-              className="px-4 py-1 mt-2 border bg-slate-200 rounded-md outline-none focus:ring-2 focus:ring-blue-400 transition text-sm font-normal w-full"
-              type="number"
-              placeholder="Quantity"
-              defaultValue={1}
-            />
-          </div>
-        </div>
-      </div>
-      <div className="footer flex justify-end items-center gap-4">
-        <button className="px-4 py-[6px] bg-red-600 text-white rounded-md">
-          Cancel
-        </button>
-        <button className="px-4 py-[6px] bg-blue-600 text-white rounded-md">
-          Issue Item
-        </button>
-      </div>
-    </dialog>,
+                <div>
+                  <h3 className="text-sm text-gray-500 mt-2">Quantity</h3>
+                  <input
+                    className="px-4 py-1 mt-2 border bg-slate-200 rounded-md outline-none focus:ring-2 focus:ring-blue-400 transition text-sm font-normal w-full"
+                    type="number"
+                    placeholder="Quantity"
+                    defaultValue={1}
+                  />
+                </div>
+              </div>
+            </div>
+            <div className="footer flex justify-end items-center gap-4 mt-4">
+              <Button onClick={onCloseModal} variant="cancel">
+                Cansal
+              </Button>
+              <Button onClick={onCloseModal} variant="success">
+                Issue Item
+              </Button>
+            </div>
+          </motion.dialog>
+        </>
+      )}
+    </AnimatePresence>,
     document.body
   );
 }
